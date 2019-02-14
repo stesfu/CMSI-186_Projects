9c9335c6b33476cd1afda361a8307dbdccb5ada4
stesfu <41877899+stesfu@users.noreply.github.com>
2019-02-13 17:12:54 -0800

    Final code clean up

--- homework02/HighRoll.java
+++ homework02/HighRoll.java
@@ -28,21 +28,21 @@ public class HighRoll{
             if( 0 == inputLine.length() ) {
                System.out.println("enter some text!:");
             }else if('1' == inputLine.charAt(0) ){
-	           gameSet.roll();
-	           System.out.println( "The dice are rolled! \n" + gameSet.toString());
-	        }else if('2' == inputLine.charAt(0) ){
-	           System.out.println( "Enter the a number for the index of the die you want to roll"); //fix
+               gameSet.roll();
+               System.out.println( "The dice are rolled! \n" + gameSet.toString());
+            }else if('2' == inputLine.charAt(0) ){
+               System.out.println( "Enter the a number for the index of the die you want to roll"); //fix
                System.out.print( ">>" );
                int rollIndIndex = Integer.parseInt(input.readLine());
                gameSet.rollIndividual(rollIndIndex);
                System.out.println( "Your new sequence is \n" + gameSet.toString());
             }else if('3' == inputLine.charAt(0) ){
-	           System.out.println("Your score is " + gameSet.sum());
+               System.out.println("Your score is " + gameSet.sum());
             }else if('4' == inputLine.charAt(0) ){
-	           highScore = gameSet.sum();
-	           System.out.println("Your high score is saved!");
+               highScore = gameSet.sum();
+               System.out.println("Your high score is saved!");
             }else if('5' == inputLine.charAt(0) ){
-	           System.out.println("Your current high score is " + highScore);
+               System.out.println("Your current high score is " + highScore);
             }else if( 'q' == inputLine.charAt(0) ) {
                break;
             }else{
