63b78f11731d69232f9ac0bcb941fcf68db3d9a5
bjohnson05 <bjohnson05@gmail.com>
2019-05-10 16:41:47 -0700

    added evaluations for homework07

---
+++ homework07/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"DynamicChangeMaker"},{"p":"<Unnamed>","l":"DynamicChangemakerTestHarness"},{"p":"<Unnamed>","l":"Tuple"},{"p":"<Unnamed>","l":"TupleTestHarness"}]
